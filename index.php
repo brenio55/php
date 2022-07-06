4a73c35292f92dc7e01702eb20258c5344fe215d
Brenio Filho <brenio55@outlook.com>
2022-07-06 17:43:04 -0300

    Aula 18 -- require e include

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+    // include "somar.php";
+    require "somar.php"; 
+    echo somar(10, 20);
+
+    //require -> obriga que o arquivo existe e que o arquivo esteja funcionando completamente
+    //include tem como configurar uma pasta padrão de procura no php.ini
+
+
+    //o include consegue trazer arquivos de outros sites
+
+    //o require é mais interessante pra ser usado por exigir o bom funcionamento da variável
+
+    //require_once -> quando um arquivo principal e o arquivo interno possui um mesmo include, esta função permite com que o arquivo só seja chamado uma vez no servidor.
     
 ?>
