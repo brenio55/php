46775e5ac3d146b0a64c15a5f16a3daac810a8bc
Brenio Filho <brenio55@outlook.com>
2022-07-06 16:42:25 -0300

    Aula 17 -- tipos de operadores do PHP.

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-  $nome = "Gláucio";
 
-  function teste(){
-    global $nome;
-    echo $nome;
-    echo "<br>";
-  } 
-
-  function teste2(){
-    $nome = "Glauciano";
-    echo $nome;
-    echo "<br>";
-  }
-
-  teste();
-  teste2();
 ?>
