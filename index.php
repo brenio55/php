6575dbff06950d0fc28833b831f672d56787842a
Brenio Filho <brenio55@outlook.com>
2022-07-07 15:13:06 -0300

    git rebase

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <form>
-    <input type="text" name="nome" id="">
-    <input type="date" name="nascimento" id="">
-    <input type="submit" value="OK">
+
 </form>
 <?php
 
-if (isset($_GET)){
-
-
-   foreach ($_GET as $key => $value){
-        echo "Nome do campo: ". $key;
-        echo "<hr>";
-        echo "Valor do campo: " .$value;
-        echo "<hr>";
-   }
-}
 ?>
