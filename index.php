ecb1e2e524dda35bd645f9226dfd3e7e852e9529
Brenio Filho <brenio55@outlook.com>
2022-07-13 19:45:40 -0300

    Aula 27 - Arrays Constantes

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
-    define('SERVIDOR', '127.0.0.1');
+    define('BANCO_DE_DADOS', [
+        '127.0.0.1',
+        'root',
+        'password',
+        'txt'
+    ]); // caso quisesse que ela fosse case sensitive, ao final do colchete, adicionava-se um parametro true;
 
-    echo SERVIDOR;
+    print_r(BANCO_DE_DADOS);
 ?>
