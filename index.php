12f780cfe8dc454b103ac003ced6be7f1154c335
Brenio Filho <brenio55@outlook.com>
2022-07-13 19:39:24 -0300

    Aula 26 - JSON DECODE

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php 
-    $pessoas = array();
-    array_push(
-        $pessoas, array(
-        'nome'=>'João',
-        'idade'=>20
-        )
-    );
-    array_push(
-        $pessoas, array(
-        'nome'=>'Glaucio',
-        'idade'=>25
-        )
-    );
+    $json = '[{"nome":"Jo\u00e3o","idade":20},{"nome":"Glaucio","idade":25}]';
 
-    echo json_encode($pessoas)
+    $data = json_decode($json);
+    var_dump($data);
 ?>
