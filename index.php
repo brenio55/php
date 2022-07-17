5981dd112525b913cdcfa46cf7b9f1117b40944b
Brenio Filho <brenio55@outlook.com>
2022-07-16 21:37:12 -0300

    Aula 31 - Funções Session

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
     session_start();
-    echo session_id();
+    echo session_id()."<br>";
 
-    echo "<a href='novoReg.php'>Gerar novo registro</a>";
+    echo "<a href='novoReg.php'>Gerar novo registro</a><br>";
+    echo session_save_path(); //retorna onde está salvo o arquivo temporário de sessão dentro do PC.
+
+    
 ?>
