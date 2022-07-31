9b8861c54fd83a8ab3929541e0c7809fd65d1e49
Brenio Filho <brenio55@outlook.com>
2022-07-31 01:04:36 -0300

    Aula 45 - Métodos Mágicos

--- index.php
+++ index.php
@@ -1,62 +1,42 @@
 <?php 
-class Documento {
-    private $numero;
+    class Endereco {
+        private $logradouro;
+        private $numero;
+        private $cidade;
 
-    public function getNumero(){
-        return $this->numero;
-    }
-
-    public function setNumero($numero){
-        $resultado = Documento::validarCPF($numero); //para acessar métodos estáticos, usa-se os dois pontos.
-
-        if ($resultado === false){
-            throw new Exception("CPF informado não é válido", 1);
+        public function __construct($a, $b, $c){
+            $this->logradouro = $a;
+            $this->numero = $b;
+            $this->cidade = $c;
         }
 
-        $this->numero = $numero;
-    }
+        public function __destruct(){
+            var_dump('Destruir');
+        }
 
-    public static function validarCPF($cpf):bool{
-        if(empty($cpf)) return false; //verifica se está em branco
+        public function __toString(){
+            return $this->logradouro. ", ".$this->numero. ", ".$this->cidade;
+        } //quando derem um ECHO no objeto diretamente, o método mágico to string será executado.
 
-        //Elimina a possível máscara
-        $cpf = preg_replace('[^0-9]', '', $cpf);
-        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+        public function getLogradouro(){
 
-        //Verifica se o length é igual a 11
-        //verifica se foi digititada esta sequência inválida de caracteres;
-        if (strlen($cpf) != 11){
-            return false;
-        }else if($cpf == '00000000000' || 
-        '11111111111' ||
-        '22222222222' ||
-        '33333333333' ||
-        '44444444444' ||
-        '55555555555' ||
-        '66666666666' ||
-        '77777777777' ||
-        '88888888888' ||
-        '99999999999'  ){
-            return false;
-        } else {
-            //calcula os dígitos verificadores para verificar se o CPF é válido;
-            for ($t = 9; $t < 11; $t++){
-                for ($d = 0, $c = 0; $c < $t; $c++){
-                    $d += $cpf($c) * (($t + 1) - $c);
-                }
-                $d = ((10 * $d) % 11) %10;
-                if ($cpf($c) != $d) {
-                    return false;
-                }
-            }
+        }
 
-            return true;
+        public function getNumero(){
+            
+        }
+
+        public function getCidade(){
+            
         }
     }
-}
 
-$cpf = new Documento();
-$cpf->setNumero("09028578439");
+    $meuEndereco = new Endereco('Rua Valdemar Saraiva Leão', '123', 'Santos');
 
-var_dump($cpf->getNumero());
+    var_dump($meuEndereco);
+    echo '<br>';
+    echo $meuEndereco;
+    echo '<br>';
+    unset($meuEndereco);
+    
 ?>
