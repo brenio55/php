a7057cd59cf4eba13ce26b2fdbb032ba7b645ff6
Brenio Filho <brenio55@outlook.com>
2022-07-31 00:50:28 -0300

    Métodos estáticos

--- index.php
+++ index.php
@@ -1,48 +1,62 @@
 <?php 
-    class Carro {
-        private $modelo;
-        private $motor;
-        private $ano;
+class Documento {
+    private $numero;
 
-        public function getModelo(){
-            return $this->modelo;
-        }
-
-        public function setModelo($modelo){
-            $this->modelo = $modelo;
-        }
-
-        public function getMotor():float{
-            return $this->motor;
-        }
-
-        public function setMotor($motor){//o retorno deste método é float;
-            $this->motor = $motor;
-        }
-
-        public function getAno():int{ //o retorno dessa função é em inteiro;
-            return $this->ano;
-        }
-
-        public function setAno($ano){
-            $this->ano = $ano;
-        }
-
-        public function exibir(){
-            return array(
-                "modelo"=>$this->getModelo(),
-                "motor"=>$this->getMotor(),
-                "ano"=>$this->getAno()
-            );
-        }
+    public function getNumero(){
+        return $this->numero;
     }
 
-    $gol = new Carro();
-    $gol->setModelo("Gol GT");
-    $gol->setMotor("1.6");
-    $gol->setAno("2017");
+    public function setNumero($numero){
+        $resultado = Documento::validarCPF($numero); //para acessar métodos estáticos, usa-se os dois pontos.
 
-    print_r($gol->exibir());
-    echo "<br>";
-    var_dump($gol->exibir());
+        if ($resultado === false){
+            throw new Exception("CPF informado não é válido", 1);
+        }
+
+        $this->numero = $numero;
+    }
+
+    public static function validarCPF($cpf):bool{
+        if(empty($cpf)) return false; //verifica se está em branco
+
+        //Elimina a possível máscara
+        $cpf = preg_replace('[^0-9]', '', $cpf);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+        //Verifica se o length é igual a 11
+        //verifica se foi digititada esta sequência inválida de caracteres;
+        if (strlen($cpf) != 11){
+            return false;
+        }else if($cpf == '00000000000' || 
+        '11111111111' ||
+        '22222222222' ||
+        '33333333333' ||
+        '44444444444' ||
+        '55555555555' ||
+        '66666666666' ||
+        '77777777777' ||
+        '88888888888' ||
+        '99999999999'  ){
+            return false;
+        } else {
+            //calcula os dígitos verificadores para verificar se o CPF é válido;
+            for ($t = 9; $t < 11; $t++){
+                for ($d = 0, $c = 0; $c < $t; $c++){
+                    $d += $cpf($c) * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) %10;
+                if ($cpf($c) != $d) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
+$cpf = new Documento();
+$cpf->setNumero("09028578439");
+
+var_dump($cpf->getNumero());
 ?>
