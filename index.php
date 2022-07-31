cf7a1ba10b1efe2189ab5c3d53eaa2aa33a67766
Brenio Filho <brenio55@outlook.com>
2022-07-31 00:04:40 -0300

    Aula 40 - CLASSES, MÉTODOS, INSTÂNCIAS E ATRIBUTOS

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php 
-$dt = new DateTime();
 
-$periodo = new DateInterval("P15D"); //periodo de 15 dias
-
-echo $dt->format("d/m/y h:i:s");
-
-$dt->add($periodo);
-
-echo "<br>";
-
-echo $dt->format("d/m/y h:i:s");
 ?>
