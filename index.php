2c1362ad7ae2f90db4761ee8f6b392d6aff90b23
Brenio Filho <brenio55@outlook.com>
2022-09-20 15:59:33 -0300

    Aula 60 - Inserindo no banco PDO

--- index.php
+++ index.php
@@ -6,21 +6,16 @@ ini_set('display_startup_errors', 1);
 
 error_reporting(E_ALL);
 
+// $conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "root");
 
-$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");//TIPO DE BANCO; IP/HOST; USUÁRIO; SENHA;
+$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
 
-$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+$login = "jose";
+$password = "12345678";
+
+$stmt->bindParam(":LOGIN", $login);
+$stmt->bindParam(":PASSWORD", $password);
 
 $stmt->execute();
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC); // os dois pontos são usados quando queremos acessar propriedades, métodos e constantes de nível de classe
-
-foreach($results as $row) {
-    foreach($row as $key => $value){
-        echo "<strong>".$key."</strong> ".$value."<br/>";
-    }
-    echo "========================================================<br/>";
-}
-
-var_dump($results);
 ?>
