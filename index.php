c6d4e51d4e77a21851b3feb59c5d62589b57d7e2
Brenio Filho <brenio55@outlook.com>
2022-09-20 16:08:32 -0300

    Aula 61 - Atualizando informações no banco PDO

--- index.php
+++ index.php
@@ -9,13 +9,21 @@ error_reporting(E_ALL);
 // $conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "root");
 
 $conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
-$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
+$stmt = $conn->prepare("UPDATE tb_usuarios 
+                        SET deslogin = :LOGIN, 
+                            dessenha = :PASSWORD 
+                        WHERE idusuario = :ID");
 
-$login = "jose";
-$password = "12345678";
+$login    = "João";
+$password = "QUERTY";
+$id       = 2;
+
 
 $stmt->bindParam(":LOGIN", $login);
 $stmt->bindParam(":PASSWORD", $password);
+$stmt->bindParam(":ID", $id);
 
 $stmt->execute();
+
+echo "Alterado OK!";
 ?>
