abaabc7625934903168e92fe0d28119fd7371bf2
brenio55 <brenio55@outlook.com>
2022-11-06 21:24:15 -0300

    Aula 73 - Lendo arquivos com FGETS (CSV)

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php 
 
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+
     spl_autoload_register(function($class_name){
         $filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
         if (file_exists($filename)){
--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php 
-    $file = fopen("teste.txt", "w+");
+    require('config.php');
 
-    fclose($file);
+    $filename = "usuarios.csv";
 
-    unlink("teste.txt"); //remove um arquivo
+    if (file_exists($filename)){
+        $file = fopen($filename, "r");
 
-    echo "Arquivo removido com sucesso!";
+        $headers = explode(",", fgets($file)); //transforma uma série de dados em uma array. Primeiro dado: separador. Segundo dado: resource.
+
+        $data = [];
+
+        while ($row = fgets($file)){ //retornando false (vazio), o while é finalizado
+            $rowData = explode(",", $row);
+            $linha = [];
+
+            for ($i = 0; $i < count($headers); $i++){
+                $linha[$headers[$i]] = $rowData[$i];
+            }
+
+            array_push($data, $linha);
+
+        }
+
+        fclose($file);
+
+        echo json_encode($data);
+    }
 ?>
