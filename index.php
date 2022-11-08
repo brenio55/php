08916c985b7332df00c6fc7e86f91a6fb4238c4a
brenio55 <brenio55@outlook.com>
2022-11-07 22:45:00 -0300

    Aula 75 - Download de arquivos externos com file_get_contents

--- index.php
+++ index.php
@@ -1,38 +1,18 @@
-<form method="POST" enctype="multipart/form-data"> <!-- //este enctype irá dizer que o conteúdo dos arquivos enviados serão em binário -->
-
-    <input type="file" name="fileUpload" id="">
-    <button type="submit">Send</button>
-
-</form>
-
-
-
 <?php 
     require('config.php');
 
-   if ($_SERVER["REQUEST_METHOD"] === "POST"){
-        $file = $_FILES["fileUpload"]; //ESTA VARIÁVEL SUPERGLOBAL SERVE PARA OS ARQUIVOS ENVIADOS POR UPLOAD
 
-        if ($file["error"]){
-            throw new Exception("Error: ".$file["error"]);
-        }
-   }
+    $link = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/640px-Google_Images_2015_logo.svg.png";
+    $content = file_get_contents($link);
 
-   //ao enviar arquivos, são criados diretórios temporários para os pacotes de recebimento, dentro do servidor PHP.
+    $parse = parse_url($link);
 
-   //é bom que seja um diretório a parte, pois caso seja enviado um arquivo PHP ao servidor, ele pode ser executado lá dentro, quebrando a segurança.
+    $basename = basename($parse['path']); //retorna o nome do index numa URL.
 
-   $dirUploads = "uploads"; 
+    $file = fopen($basename, "w+");
+    fwrite($file, $content);
+    fclose($file);
 
-   if (!is_dir($dirUploads)){
-        mkdir($dirUploads);
-   }
-
-   if (move_uploaded_file($file["tmp_name"], $dirUploads.DIRECTORY_SEPARATOR.$file["name"])){
-    echo "Upload realizado com sucesso";
-    }else{
-        throw new Exception ("Não foi possível realizar o upload.");
-    }
-
-   
 ?>
+
+<img src="<?=$basename?>" alt="" />
